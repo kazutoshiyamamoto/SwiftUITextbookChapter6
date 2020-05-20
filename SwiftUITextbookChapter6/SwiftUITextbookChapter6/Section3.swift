828bf4ec3f69e5ec26d88062658f2f6d31e713a7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-20 11:47:36 +0900

    ボタンを追加

---
+++ SwiftUITextbookChapter6/SwiftUITextbookChapter6/Section3.swift
@@ -0,0 +1,27 @@
+//
+//  Section3.swift
+//  SwiftUITextbookChapter6
+//
+//  Created by home on 2020/05/20.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section3: View {
+    @State var isModal: Bool = false
+    
+    var body: some View {
+        Button(action: {
+            self.isModal = true
+        }) {
+            Text("Sheet テスト")
+        }
+    }
+}
+
+struct Section3_Previews: PreviewProvider {
+    static var previews: some View {
+        Section3()
+    }
+}
