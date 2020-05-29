b68073dd59e62a1e414769cfd6bcb4e67fb041cf
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-05-29 23:54:57 +0900

    タブを表示するSwiftUIファイルを追加

---
+++ SwiftUITextbookChapter6/SwiftUITextbookChapter6/Section5.swift
@@ -0,0 +1,21 @@
+//
+//  Section5.swift
+//  SwiftUITextbookChapter6
+//
+//  Created by home on 2020/05/29.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct Section5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Section5_Previews: PreviewProvider {
+    static var previews: some View {
+        Section5()
+    }
+}
