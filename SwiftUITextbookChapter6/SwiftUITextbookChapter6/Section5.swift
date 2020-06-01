43fdfc1e1da584b83bee9aca4af191d3b610cfd0
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-06-01 23:07:39 +0900

    タブを追加

--- SwiftUITextbookChapter6/SwiftUITextbookChapter6/Section5.swift
+++ SwiftUITextbookChapter6/SwiftUITextbookChapter6/Section5.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct Section5: View {
+    @State private var selection = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView(selection: $selection) {
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+            }
+            .tag(0)
+            
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+            }
+            .tag(1)
+        }
     }
 }
 
